76146d7651619f0ba7e2d2a6a27b071642f40a26
W. Max Lees <william.lees@echostar.com>
2015-06-26 21:20:53 -0600

    Added result readout

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,28 @@ fn main() {
     println!("\nNow watching ./src");
     t_out.reset();
 
+
     let (tx, rx) = channel();
     let w: Result<RecommendedWatcher, Error> = Watcher::new(tx);
+
 
     match w {
 
         Ok(mut watcher) =>  {
-            let _ = watcher.watch(&Path::new("./src/"));
+            match watcher.watch(&Path::new("./src/")) {
+                Ok(_) => {},
+                Err(e) => {
+                    println!("Error creating watcher: {:?}", e);
+                    return
+                },
+            }
 
             loop {
                 thread::sleep_ms(5000);
 
                 match rx.recv() {
                     Ok(e) => handle(e),
-                    Err(_) => (),
+                    Err(e) => println!("{:?}", e),
                 }
             }
         },
@@ -36,16 +44,26 @@ fn main() {
     }
 }
 
-
 fn handle(e: notify::Event) {
     match e.op {
         Ok(_) => {
+            let mut t_out = term::stdout().unwrap();
+
             let output = Command::new("cargo")
                                     .arg("test")
                                     .output()
                                     .unwrap_or_else(|e| { panic!("Failed to execute cargo: {}", e)});
                                     
             println!("{}", String::from_utf8_lossy(&output.stdout));
+            println!("{}", String::from_utf8_lossy(&output.stderr));
+            if (output.status.success()) {
+                t_out.fg(term::color::GREEN);
+                println!("\n\n\nTEST SUCCESSFUL\n\n\n");
+            } else {
+                t_out.fg(term::color::RED);
+                println!("\n\n\nTEST FAILED\n\n\n");
+            }
+            t_out.reset();
         },
         Err(e) => println!("{:?}", e),
     }
